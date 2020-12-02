17dd8a1a40ad6489a1e41d36d546891903766b55
shmakovpn <shmakovpn@yandex.ru>
2020-12-02 07:44:32 +0300

    fixed issue with HttpClient module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { SelectorComponent } from './selector/selector.component';
 import { OrganizerComponent } from './organizer/organizer.component';
 import { MomentPipe } from './shared/moment.pipe';
-import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   imports:      [ 
     BrowserModule, 
     FormsModule, // 53:51
     ReactiveFormsModule, // 53:58
-    HttpClientModule, // https://stackoverflow.com/questions/50282848/cant-find-angular-common-http-on-stackblitz
+    HttpClientModule, // 59:24 https://stackoverflow.com/questions/50282848/cant-find-angular-common-http-on-stackblitz
   ],
   declarations: [ 
     AppComponent, 
@@ -22,7 +23,6 @@ import { HttpClientModule } from '@angular/common/http';
     SelectorComponent, 
     OrganizerComponent, 
     MomentPipe, // 23:11
-    HttpClientModule, // 59:24
   ], 
   bootstrap:    [ AppComponent ]
 })
